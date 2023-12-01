256b7a818ca2b7a984bed128ff96cc2255e7ac62
Tarunteja Obbina <tarun@Taruntejas-MacBook-Pro.local>
2023-12-01 16:29:48 -0600

    add backward method to maxpool

--- src/layers/MaxPool.py
+++ src/layers/MaxPool.py
@@ -6,8 +6,10 @@ class MaxPool:
     self.strides = strides
     self.padding = padding
     self.device = device
+    self.input
   
-  def forward(self, X):
+  def forward(self, X: torch.Tensor) -> torch.Tensor:
+    self.input = X
     y = torch.zeros(X.shape[0], X.shape[1], int((X.shape[2] - self.kernel_size[0] + (2 * self.padding))/self.strides) + 1, int((X.shape[3] - self.kernel_size[1] + (2 * self.padding))/self.strides) + 1, device=self.device)
     if self.padding > 0:
       new_size = (X.shape[0], X.shape[1], X.shape[2] + 2*self.padding, X.shape[3] + 2*self.padding)
@@ -15,12 +17,33 @@ class MaxPool:
       x_padded[:, :, self.padding: x_padded.shape[2]-self.padding, self.padding: x_padded.shape[3]-self.padding] = X
       X = x_padded
 
-    for c in range(0, y.shape[3], self.strides):
-      col_start = c
-      col_end = c + self.kernel_size[1]
-      for r in range(0, y.shape[2], self.strides):
-        row_start = r
-        row_end = r + self.kernel_size[0]
-        y[:, :, row_start, col_start] = (torch.max((X[:, :, row_start: row_end, col_start: col_end]).reshape(X.shape[0], X.shape[1], 1, -1), dim=3)).values.reshape(X.shape[0], -1)
+    for c in range(0, y.shape[3]):
+      col_start = c * self.strides
+      col_end = c * self.strides + self.kernel_size[1]
+      for r in range(0, y.shape[2]):
+        row_start = r * self.strides
+        row_end = r * self.strides + self.kernel_size[0]
+        y[:, :, r, c] = (torch.max((X[:, :, row_start: row_end, col_start: col_end]).reshape(X.shape[0], X.shape[1], 1, -1), dim=3)).values.reshape(X.shape[0], -1)
     
     return y
+
+  def backward(self, grad: torch.Tensor) -> torch.Tensor:
+    assert self.input != None
+    grad_input_tensor = torch.zeros_like(self.input)
+    
+    for c in range(0, grad.shape[3]):
+      col_start = c * self.strides
+      col_end = c * self.strides + self.kernel_size[1]
+      for r in range(0, grad.shape[2]):
+        row_start = r * self.strides
+        row_end = r * self.strides + self.kernel_size[0]
+
+        input_slice = self.input[:, :, row_start:row_end, col_start: col_end]
+        max_positions = torch.argmax(input_slice.view(self.input.shape[0], self.input.shape[1], -1), dim=2)
+        
+        max_i = max_positions // self.kernel_size[0]
+        max_j = max_positions % self.kernel_size[0]
+
+        grad_input_tensor[:, :, row_start: row_end, col_start: col_end][:, :, max_i, max_j] += grad[:, :, r, c]
+    
+    return grad_input_tensor
