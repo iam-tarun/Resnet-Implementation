1ddd4d6a118a8464a14e18edfc4ba7e550f2ac63
Tarunteja Obbina <tarun@Taruntejas-MacBook-Pro.local>
2023-12-01 17:29:42 -0600

    add linear layer

---
+++ src/layers/Linear.py
@@ -0,0 +1,31 @@
+import torch
+
+class Linear:
+  def __init__(self, in_features: int, out_features: int, device="cpu"):
+    self.device = device
+    self.in_features = in_features
+    self.out_features = out_features
+    self.weight = torch.randn(self.out_features, self.in_features, device=self.device)
+    self.bias = torch.randn(self.out_features, device=self.device)
+    self.input = None
+
+  # shape of X should be (batch_size, n_features)
+  def forward(self, X: torch.Tensor) -> torch.Tensor:
+    assert len(X.shape) == 2
+    self.input = X
+    return X.dot(self.weight) + self.bias.expand(X.shape[0], self.out_features)
+  
+  def backward(self, grad: torch.Tensor, lr: float = 0.01):
+    assert self.input != None
+    assert self.out_features == grad.shape[1]
+    assert self.input.shape[0] == grad.shape[0]
+    
+    grad_weight = torch.dot(grad.T, self.input)
+    grad_bias = torch.sum(grad, dim=0)
+
+
+    self.weight -= lr * grad_weight
+    self.bias -= lr * grad_bias
+
+    return torch.dot(grad, self.weight)
+  
